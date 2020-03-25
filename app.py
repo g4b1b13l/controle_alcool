7162d4b1aa3b36e72e48055c1f5263ffbda1f3bf
g4b1b13l <56329206+g4b1b13l@users.noreply.github.com>
2020-03-24 22:11:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ external_stylesheets = ['https://codepen.io/g4b1b13l/pen/VwwrYdL.css'] # Esse eh
 
 app = dash.Dash(__name__, external_stylesheets=[external_stylesheets,'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'],
         meta_tags=[
-        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+        {"name": "viewport", "content": "width=device-width, initial-scale=0"}
     ])
 server = app.server
 mydb=psy.connect (
